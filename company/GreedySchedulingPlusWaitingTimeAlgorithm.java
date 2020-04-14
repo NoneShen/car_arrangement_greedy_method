61fb0dbea4c6dd226d63cdd5bd62e4b4cb0f3b36
NoneShen <45577810+NoneShen@users.noreply.github.com>
2020-04-14 15:44:07 -0400

    Update GreedySchedulingPlusWaitingTimeAlgorithm.java

--- company/GreedySchedulingPlusWaitingTimeAlgorithm.java
+++ company/GreedySchedulingPlusWaitingTimeAlgorithm.java
@@ -9,21 +9,18 @@ public class GreedySchedulingPlusWaitingTimeAlgorithm extends TripSchedulingAlgo
     public static int TripSchedule(Car_list car_list, Trip trip,int max_waiting_time){
         int size=car_list.car_list.size();//number of cars
         int[] a=new int[size];//distance between each car and the customer
-        for(int i=0;i<size;i++){
-            Car car=car_list.car_list.get(i);
-            a[i]=calculate_distance(car.position,trip.getSrc());
-        }
-        int[] index=sort_array(a);
         int time_from_car_to_customer=0;
         int time_remain=0;
         int sign=0;
         int temp=0;
-        Car car=new Car();
+        for(int i=0;i<size;i++){
+            Car car=car_list.car_list.get(i);
+            a[i]=calculate_distance(car.position,trip.getSrc())+Math.max(car.getTime_stamp_arrive_des()-trip.getStartTime()-max_waiting_time,0);
+        }
+        int[] index=sort_array(a);
+
         while(sign==0&&temp<size){
-            car= car_list.car_list.get(index[temp]);
-            time_from_car_to_customer =calculate_distance(car.getPosition(),trip.getSrc());
-            time_remain=Math.max(car.getTime_stamp_arrive_des()-trip.getStartTime(),0);
-            if(time_from_car_to_customer+time_remain<max_waiting_time){
+            if(a[index[temp]]<max_waiting_time){
                 sign=1;
                 temp--;
             }
@@ -33,9 +30,12 @@ public class GreedySchedulingPlusWaitingTimeAlgorithm extends TripSchedulingAlgo
             System.out.println("Can not serve this customer, because there is not a car available in "+ max_waiting_time+"s.");
             return -1;
         }
+        time_from_car_to_customer=calculate_distance(car_list.car_list.get(index[temp]).position,trip.getSrc());
+        time_remain= Math.max(car_list.car_list.get(index[temp]).getTime_stamp_arrive_des()-trip.getStartTime(),0);
         trip.calculate_end_time(time_from_car_to_customer+time_remain);
         car_list.car_list.get(index[temp]).setTime_stamp_arrive_des(trip.getEnd_time());
         car_list.car_list.get(index[temp]).setPosition(trip.getDest());
+      //  System.out.println(index[temp]);
         return index[temp];
     }
     static void simulate(Car_list car_list, Customer_demand demand, int n, int max_waiting_time){
@@ -64,6 +64,7 @@ public class GreedySchedulingPlusWaitingTimeAlgorithm extends TripSchedulingAlgo
         System.out.println("Total Cost: "+total_cost);
         System.out.println("Total revenue: "+total_revenue);
         System.out.println("Total customer: "+Count);
+        System.out.println("Average Cost:" + (total_cost-total_revenue)/Count);
         System.out.println("*************************************************************************************************");
     }
 }
